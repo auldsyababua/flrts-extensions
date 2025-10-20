954c7f591591ddd3314fe63163fed5e595395f15
Colin Aulds <18078607+auldsyababua@users.noreply.github.com>
2025-10-20 11:40:54 -0700

    feat(schema): add Mining Site, Contractor DocTypes + Maintenance Visit custom fields (10N-256)
    - Add Mining Site master DocType (flat structure, 4 fields)
    - Add Contractor master DocType (5 core fields)
    - Add 7 custom fields to Maintenance Visit:
      - supabase_task_id (future reconciliation)
      - flrts_owner (Link: User)
      - flrts_priority (Select: 1-5)
      - flrts_site (Link: Mining Site)
      - flrts_contractor (Link: Contractor)
      - flrts_metadata (JSON)
      - custom_synced_at (Datetime)
    
    Closes 10N-256

---
+++ flrts_extensions/flrts_extensions/doctypes/contractor/contractor.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, 10N Zero and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+class Contractor(Document):
+    """
+    Contractor master DocType for tracking maintenance service providers.
+
+    See docs/.scratch/10n-256/implementation-plan.md for schema details.
+    """
+    pass
